b661a9a0f8762e2a28c85d2b9c31705cace3a54d
ebeth104 <109683183+ebeth104@users.noreply.github.com>
2023-12-16 02:27:30 +0530

    added functions

--- quizz-app-backend/src/main/java/com/example/lisa/QuizApp/service/QuestionService.java
+++ quizz-app-backend/src/main/java/com/example/lisa/QuizApp/service/QuestionService.java
@@ -2,6 +2,7 @@ package com.example.lisa.QuizApp.service;
 
 import com.example.lisa.QuizApp.models.Question;
 import com.example.lisa.QuizApp.dao.QuestionDao;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -43,4 +44,40 @@ public class QuestionService {
         }
         return new ResponseEntity<>("Database Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    public Question getQuestionById(int id) {
+        return questionDao.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Question not found"));
+    }
+
+    public ResponseEntity<String> deleteQuestion(int id) {
+        try {
+            Question question = getQuestionById(id);
+            questionDao.delete(question);
+            return new ResponseEntity<>("Deleted successfully", HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Not found", HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<String> updateQuestion(int id, Question updatedQuestion) {
+        try {
+            Question question = getQuestionById(id);
+            question.setTitle(updatedQuestion.getTitle());
+            question.setOption1(updatedQuestion.getOption1());
+            question.setOption2(updatedQuestion.getOption2());
+            question.setOption3(updatedQuestion.getOption3());
+            question.setOption4(updatedQuestion.getOption4());
+            question.setRightAnswer(updatedQuestion.getRightAnswer());
+            question.setCategory(updatedQuestion.getCategory());
+            question.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
+            questionDao.save(question);
+            return new ResponseEntity<>("success",HttpStatus.CREATED);
+        } catch(DataAccessException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Database Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
+   
